bd16702cd8a42e5a42d10ad4ef3013ea79e35b26
roddms <122669430+roddms@users.noreply.github.com>
2025-05-18 09:46:33 +0900

    [Silver III] Title: 수열, Time: 88 ms, Memory: 41676 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,14 +1,17 @@
+#2559
+
 N, K = map(int, input().split())
-tem = list(map(int, input().split()))
 
-prefix = [0] * (N + 1)
+tem = list(map(int,input().split()))
 
-for i in range(1, N + 1):
-    prefix[i] = prefix[i - 1] + tem[i - 1]
+prefix = [0 for _ in range(N+1)]
+
+for i in range(1, N+1):
+    prefix[i] = prefix[i-1] + tem[i-1]
 
 arr = []
 
-for i in range(K, N + 1):  # ✅ 반드시 N+1까지!
-    arr.append(prefix[i] - prefix[i - K])
+for i in range(K, N+1):
+    arr.append(prefix[i] - prefix[i-K])
 
 print(max(arr))
