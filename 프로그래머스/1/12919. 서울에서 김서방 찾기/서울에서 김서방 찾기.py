6362ccd3241fb9c99ac3b68a57979bda7aa6c5f1
roddms <122669430+roddms@users.noreply.github.com>
2025-08-18 10:03:09 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.02 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,9 +1,4 @@
 def solution(seoul):
-    answer = ''
-    
-    for i in range(0, len(seoul)+1) :
-        if seoul[i] == 'Kim' :
-            answer = i
-            break;
-    
-    return f"김서방은 {answer}에 있다"
+    for i in range(len(seoul)):
+        if seoul[i] == 'Kim':
+            return f"김서방은 {i}에 있다"
