b22d434aa2a6521d4a06d43d70611e93491c0b49
roddms <122669430+roddms@users.noreply.github.com>
2025-11-11 21:09:18 +0900

    [level 2] Title: 타겟 넘버, Time: 570.97 ms, Memory: 98.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -3,18 +3,18 @@ from collections import deque
 def solution(numbers, target):
     answer = 0
     
-    q = deque
-    q = deque([(0, 0)]) # 인덱스, 현재까지의 합
+    q = deque([(0, 0)])
     
-    while q:
+    while q:  
         idx, cur = q.popleft()
         
         if idx == len(numbers):
             if cur == target:
                 answer += 1
             continue
-        
-        q.append((idx+1, cur+numbers[idx]))
-        q.append((idx+1, cur-numbers[idx]))            
+            
+        q.append((idx+1, cur + numbers[idx]))
+        q.append((idx+1, cur - numbers[idx]))
+    
     
     return answer
