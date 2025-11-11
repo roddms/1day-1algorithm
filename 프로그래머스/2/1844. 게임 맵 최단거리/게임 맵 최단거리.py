eba2689b8ef821aecd8741ec239477d4a596c7d2
roddms <122669430+roddms@users.noreply.github.com>
2025-11-11 21:24:14 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.31 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -3,28 +3,27 @@ from collections import deque
 def solution(maps):
     n, m = len(maps), len(maps[0])
     
-    q = deque()
-    q = deque([(0, 0)])
-    
-    if maps[0][0] == 0 or maps[n-1][m-1] == 0:
-        return -1
+    q = deque([(0,0)])
     
     dist = [[0]*m for _ in range(n)]
     dist[0][0] = 1
     
+    if maps[0][0]==0 or maps[n-1][m-1]==0:
+        return -1
+    
     dirs = [(0,1), (0,-1), (1,0), (-1,0)]
     
     while q:
-        x, y = q.popleft()
+        x,y = q.popleft()
         
-        if x==n-1 and y==m-1:
+        if x == n-1 and y==m-1:
             return dist[x][y]
         
         for dx, dy in dirs:
-            nx, ny = x+dx, y+dy
-
-            if 0 <= nx <n and 0 <= ny <m and maps[nx][ny] == 1 and dist[nx][ny] == 0:
-                dist[nx][ny] = dist[x][y] + 1
-                q.append((nx, ny))
+            nx = dx+x
+            ny = dy+y
+            if 0<=nx<n and 0<=ny<m and maps[nx][ny]==1 and dist[nx][ny]==0:
+                dist[nx][ny] = dist[x][y]+1
+                q.append((nx,ny))
     
     return -1
